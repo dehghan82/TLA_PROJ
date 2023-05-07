26b9f29c091e16a8888ef04c5c094fc7279a1f3a
dehghan82 <dehghanzahra1400@gmail.com>
2023-05-07 17:35:34 +0430

    Phase2 updated

--- Phase2/program2.cs
+++ Phase2/program2.cs
@@ -15,7 +15,7 @@ class Program
         //Read from a Json File and convert to FA
 
         //var d = File.ReadAllText(@"E:\TLA01-Projects\samples\phase2-sample\in\input1.json");
-        var d = File.ReadAllText(@"C:\Users\Lenovo\Documents\TLA Dr Entezari\TLA01-Projects\samples\phase2-sample\in\input1.json");
+        var d = File.ReadAllText(@"..\Results\phase2-sample\in\input1.json");
         var fa_in = JsonSerializer.Deserialize<FA>(d);
         IAutomata q = fa_in.set();
         DFA dfa = q.to_DFA();
@@ -24,36 +24,12 @@ class Program
         FA fa_out = new_dfa.SetR();
         string jason = JsonSerializer.Serialize(fa_out, new JsonSerializerOptions { WriteIndented = true });
         jason = Regex.Unescape(jason);
-        File.WriteAllText(@"C:\Users\Lenovo\Documents\TLA Dr Entezari\test.json", jason, Encoding.UTF8);
+        File.WriteAllText(@"..\Results\phase2-sample\out\RFA.json", jason, Encoding.UTF8);
     }
     static public DFA simplify_DFA(DFA dfa)
     {
         int n = dfa._states.Count;
         bool[,] table = new bool[n, n];
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                table[i, j] = false;
-            }
-        }//staet initializing table
-         //for (int i = 0; i < n; i++)
-         //{
-         //    for (int j = 0; j < dfa._states[i].dtransitions.Count; j++)
-         //    {
-         //        var x = dfa._states[i].dtransitions.ElementAt(j);
-         //        int q = dfa._states.FindIndex(y => y.Name == x.Value.Name);// return the index of the state that its name is equal to name of the adj state
-         //        for(int z = 0; z < dfa._final_states.Count; z++)
-         //        {
-         //            if(dfa._final_states[z].Name == dfa._states[q].Name || dfa._final_states[z].Name==dfa._states[i].Name)
-         //            {
-         //                table[i, q] = true;
-         //                break;
-         //            }
-         //        }
-
-        //    }
-        //}//end initializing
         for (int j = 0; j < n; j++)
         {
 
@@ -62,15 +38,18 @@ class Program
                 int cnt = 0;
                 if (dfa._final_states.Contains(dfa._states[j]))
                 {
-                    cnt++;
+                    if (!dfa._final_states.Contains(dfa._states[i]))
+                        cnt++;
                 }
                 if (dfa._final_states.Contains(dfa._states[i]))
                 {
-                    cnt++;
+                    if (!dfa._final_states.Contains(dfa._states[j]))
+                        cnt++;
                 }
                 if (cnt == 1)
                 {
                     table[i, j] = true;
+                    table[j, i] = true;
                 }
             }
         }
@@ -93,7 +72,7 @@ class Program
                             if (table[q1, q2] == true || table[q2, q1] == true)
                             {
                                 changes++;
-                                table[j, j] = true;
+                                table[i, j] = true;
                                 table[j, i] = true;
                                 break;
                             }
@@ -108,11 +87,12 @@ class Program
         for (int i = 0; i < n; i++)
         {
             combine.Add(new List<int>());
+            combine.Last().Add(i);
         }
 
         for (int j = 0; j < n; j++)
         {
-            combine[j].Add(j);
+            // combine[j].Add(j);
             for (int i = j + 1; i < n; i++)
             {
 
@@ -123,43 +103,44 @@ class Program
                 }
             }
         }
-        for (int i = 0; i < n; i++)
-        {
-            combine[i].Sort();
-        }
-        var new_states = combine.Distinct().ToList();
+        combine.ForEach(x => x.Sort());
+        var new_state = combine.ToList();
         List<State> states = new List<State>();// states of final dfa 
         List<State> final_states = new List<State>();
-        for (int i = 0; i < new_states.Count; i++)
+        for (int i = 0; i < combine.Count; i++)
         {
             string name = "";
-            for (int k = 0; k < new_states[i].Count; k++)
+            for (int k = 0; k < combine[i].Count; k++)
             {
-                name += "q" + new_states[i][k].ToString();
+                name += "q" + combine[i][k].ToString();
             }
             State state = new State(name);
-            states.Add(state);
-            string nam = "q" + new_states[i].First().ToString();
-            for (int j = 0; j < dfa._final_states.Count; j++)
+            //
+            if (states.Count(x => x.Name == name) == 0)
             {
-                if (dfa._final_states[j].Name == nam)
-                {
-                    final_states.Add(state);
-                }
+                states.Add(state);
+                states.Last().dtransitions = new Dictionary<string, State>();
             }
-        }//biulding new states with new names
-        for (int i = 0; i < new_states.Count; i++)
-        {
+            else
+                new_state.Remove(combine[i]);
 
-            var transition = dfa._states[new_states[i].First()].dtransitions;
+        }//biulding new states with new names
+        for (int i = 0; i < new_state.Count; i++)
+        {
+            var transition = dfa._states[new_state[i].First()].dtransitions;
             for (int j = 0; j < dfa._input_symbols.Count; j++)
             {
                 State x = transition[dfa._input_symbols[j]];
                 int q1 = dfa._states.FindIndex(y => y.Name == x.Name);//index of previous state
-                int qj = new_states.FindIndex(y => y.Contains(q1));//index of new state
+                int qj = new_state.FindIndex(y => y.Contains(q1));//index of new state
+                var eq = states.Where(x => x.Name.Contains($"{q1}")).First();
                 states[i].dtransitions.Add(dfa._input_symbols[j], states[qj]);
             }
         }
+        foreach (var item in states)
+            foreach (var it in dfa._final_states)
+                if (item.Name.Contains(it.Name[1]))
+                    final_states.Add(item);
 
         return new DFA(states, states[0], final_states, dfa._input_symbols);
     }
